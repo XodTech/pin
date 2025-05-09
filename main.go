96a230790696c0f8bdfabf142484ccae87fd6d1d
XodTech <82662318+XodTech@users.noreply.github.com>
2025-05-09 20:07:21 +0200

    Feature:Implemented configuration parsing

--- main.go
+++ main.go
@@ -1,7 +1,8 @@
 package main
+
 import (
 	"fmt"
-	// "strings"
+	"strings"
 	"io/ioutil"
 	"os"
 )
@@ -10,5 +11,16 @@ func main(){
 	if err != nil {
 		fmt.Println("Unable to open and read configuration file")
 		os.Exit(1)
+	}
+	config := make(map[string]string)
+	for _,pair := range strings.Split(string(conf_content),`;`){
+		kv := strings.Split(pair,`=`)
+		if len(kv) == 2 {
+			config[kv[0]] = kv[1]
+		}else if len(kv) == 1{
+			break
+		}else {
+			fmt.Printf("Invalid pair %s\n",pair)
+		}
 	}
 }
